2e7e92901a3bc093f631cd547ec6ba48116f419b
Fernando Herrera <herrera770@outlook.es>
2024-03-19 15:07:28 -0300

    se agregó la ruta estátca

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ import express from "express";
 import 'dotenv/config'
 import morgan from "morgan";
 import cors from "cors";
-
+import {fileURLToPath} from 'url';
+import path from "path";
 
 console.log("index funciona correctamente");
 
@@ -17,3 +18,8 @@ app.use(morgan('dev'));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, '/public')));
